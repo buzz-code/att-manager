69724f8341ed1c661b4f957efa532354bffc2a2d
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-10-08 14:16:23 +0300

    add filters

--- client/containers/diaries/DiariesContainer.js
+++ client/containers/diaries/DiariesContainer.js
@@ -1,7 +1,7 @@
-import React, { useMemo, useCallback } from 'react';
+import React, { useEffect, useMemo, useCallback } from 'react';
 import { getJewishDate, formatJewishDateHebrew } from 'jewish-dates-core';
 import { useHistory } from 'react-router';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Table from '../../../common-modules/client/components/table/Table';
 import * as crudAction from '../../../common-modules/client/actions/crudAction';
@@ -10,8 +10,10 @@ const getColumns = () => [
   { field: 'group_name', title: 'קבוצה' },
   { field: 'first_lesson', title: 'שיעור ראשון', render: ({ first_lesson }) => first_lesson && formatJewishDateHebrew(getJewishDate(new Date(first_lesson))) },
 ];
-const getFilters = () => [
-  // { field: 'group_name', label: 'קבוצה', type: 'text', operator: 'like' },
+const getFilters = ({ klasses, teachers, lessons }) => [
+  { field: 'klasses.name', label: 'כיתה', type: 'list', operator: 'like', list: klasses, idField: 'key' },
+  { field: 'teachers.name', label: 'מורה', type: 'list', operator: 'like', list: teachers, idField: 'tz' },
+  { field: 'lessons.name', label: 'שיעור', type: 'list', operator: 'like', list: lessons, idField: 'key' },
 ];
 const getActions = (handlePrintOne, handleOpenDiary) => [
   {
@@ -29,6 +31,9 @@ const getActions = (handlePrintOne, handleOpenDiary) => [
 const DiariesContainer = ({ entity, title }) => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const {
+    GET: { '../groups/get-edit-data': editData },
+  } = useSelector((state) => state[entity]);
 
   const handlePrintOne = useCallback((e, rowData) => {
     dispatch(crudAction.download(entity, 'POST', 'print-one-diary', { id: rowData.id, group_id: rowData.group_id }));
@@ -38,8 +43,12 @@ const DiariesContainer = ({ entity, title }) => {
   }, []);
 
   const columns = useMemo(() => getColumns(), []);
-  const filters = useMemo(() => getFilters(), []);
+  const filters = useMemo(() => getFilters(editData || {}), [editData]);
   const actions = useMemo(() => getActions(handlePrintOne, handleOpenDiary), [handlePrintOne, handleOpenDiary]);
+
+  useEffect(() => {
+    dispatch(crudAction.customHttpRequest(entity, 'GET', '../groups/get-edit-data'));
+  }, []);
 
   return <Table entity={entity} title={title} columns={columns} filters={filters} additionalActions={actions} disableAdd={true} disableUpdate={true} />;
 };
