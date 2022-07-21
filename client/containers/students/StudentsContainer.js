187c3bf710e651f6e559f658e620fd83ffaaba37
buzz-code <72219643+buzz-code@users.noreply.github.com>
2022-07-22 00:58:05 +0300

    add columns to students table

--- client/containers/students/StudentsContainer.js
+++ client/containers/students/StudentsContainer.js
@@ -5,10 +5,14 @@ import Table from '../../../common-modules/client/components/table/Table';
 const getColumns = () => [
   { field: 'tz', title: 'תעודת זהות' },
   { field: 'name', title: 'שם' },
+  { field: 'address', title: 'כתובת' },
+  { field: 'phone', title: 'טלפון' },
 ];
 const getFilters = () => [
   { field: 'tz', label: 'תעודת זהות', type: 'text', operator: 'like' },
   { field: 'name', label: 'שם', type: 'text', operator: 'like' },
+  { field: 'address', label: 'כתובת', type: 'text', operator: 'like' },
+  { field: 'phone', label: 'טלפון', type: 'text', operator: 'like' },
 ];
 
 const StudentsContainer = ({ entity, title }) => {
