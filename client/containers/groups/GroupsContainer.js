754ba25ebf483cacae4dd6247b1c3aeecbe8b5b9
buzz-code <72219643+buzz-code@users.noreply.github.com>
2022-09-06 18:58:43 +0300

    add year filter to groups container

--- client/containers/groups/GroupsContainer.js
+++ client/containers/groups/GroupsContainer.js
@@ -19,6 +19,7 @@ const getFilters = ({ klasses, teachers, lessons }) => [
   { field: 'klasses.key', label: 'כיתה', type: 'list', operator: 'eq', list: klasses, idField: 'key' },
   { field: 'teachers.tz', label: 'מורה', type: 'list', operator: 'eq', list: teachers, idField: 'tz' },
   { field: 'lessons.key', label: 'שיעור', type: 'list', operator: 'eq', list: lessons, idField: 'key' },
+  { field: 'year', label: 'שנה', type: 'list', operator: 'eq', list: yearsList, defaultValue: 5783 },
 ];
 
 const GroupsContainer = ({ entity, title }) => {
