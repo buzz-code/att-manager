ad629b7946f908a0e513d9cfbf4b62c65ef59152
buzz-code <72219643+buzz-code@users.noreply.github.com>
2023-02-27 21:35:41 +0200

    update excel file

--- server/utils/excelHelper.js
+++ server/utils/excelHelper.js
@@ -7,6 +7,10 @@ export async function getGradeExcelStreamByGroupId(groupId, half) {
     const templatePath = path.join(templatesDir, `grade.xlsx`);
     const templateData = await getDiaryDataByGroupId(groupId);
     await addGradeMetadataToTemplateData(templateData, 'דו"ח ציונים', half, false);
+    templateData.students.forEach((item, index) => {
+        item.number = index + 1;
+        item.empty = templateData.headers.map(item => '')
+    });
     const fileStream = await renderExcelTemplateToStream(templatePath, templateData);
     const filename = getGradeFilenameFromGroup(templateData.group, half);
     return { fileStream, filename };
