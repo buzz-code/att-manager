5277a79bacd15c6ecdf486f03833386be77fbec6
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2024-11-26 19:07:13 +0200

    add teacher columns

--- client/containers/excel-import/ExcelImportContainer.js
+++ client/containers/excel-import/ExcelImportContainer.js
@@ -6,7 +6,7 @@ import ExcelImport from '../../../common-modules/client/components/excel-import/
 
 const title = 'העלאת קובץ';
 const supportedEntities = [
-  { value: entities.TEACHERS, title: titles.TEACHERS, columns: ['tz', 'name', 'phone'] },
+  { value: entities.TEACHERS, title: titles.TEACHERS, columns: ['tz', 'name', 'phone', 'phone2', 'address', 'city'] },
   { value: entities.STUDENTS, title: titles.STUDENTS, columns: ['tz', 'name', 'address', 'city', 'phone', 'mother_phone', 'father_phone', 'birth_date', 'school_klass', 'father_name', 'comment', 'year'] },
   { value: entities.LESSONS, title: titles.LESSONS, columns: ['key', 'name', 'year'] },
   { value: entities.KLASSS, title: titles.KLASSS, columns: ['key', 'name', 'year'] },
--- client/containers/teachers/TeachersContainer.js
+++ client/containers/teachers/TeachersContainer.js
@@ -6,11 +6,17 @@ const getColumns = () => [
   { field: 'tz', title: 'תעודת זהות' },
   { field: 'name', title: 'שם' },
   { field: 'phone', title: 'מספר טלפון' },
+  { field: 'phone2', title: 'טלפון נייח' },
+  { field: 'address', title: 'כתובת' },
+  { field: 'city', title: 'עיר' },
 ];
 const getFilters = () => [
   { field: 'tz', label: 'תעודת זהות', type: 'text', operator: 'like' },
   { field: 'name', label: 'שם', type: 'text', operator: 'like' },
   { field: 'phone', label: 'מספר טלפון', type: 'text', operator: 'like' },
+  { field: 'phone2', label: 'טלפון נייח', type: 'text', operator: 'like' },
+  { field: 'address', label: 'כתובת', type: 'text', operator: 'like' },
+  { field: 'city', label: 'עיר', type: 'text', operator: 'like' },
 ];
 
 const TeachersContainer = ({ entity, title }) => {
